0af2005b704345d257ca7d8a8ba1712253d90d61
Riccardo8991 <75617080+Riccardo8991@users.noreply.github.com>
2024-10-25 17:05:07 +0200

    Update GildedRose.java
    Created new Function ItemNameIs

--- GildedRose.java
+++ GildedRose.java
@@ -6,13 +6,17 @@ class GildedRose {
     public GildedRose(Item[] items) {
         this.items = items;
     }
-
+    
+    public ItemNameIs(name){
+        return items[i].name.equals(name);
+    }
+    
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            if (!ItemNameIs("Aged Brie")
+                    && !ItemNameIs("Backstage passes to a TAFKAL80ETC concert")) {
                 if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                    if (!ItemNameIs("Sulfuras, Hand of Ragnaros")) {
                         items[i].quality--;
                     }
                 }
@@ -20,7 +24,7 @@ class GildedRose {
                 if (items[i].quality < 50) {
                     items[i].quality = ++;
 
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                    if (ItemNameIs("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
                                 items[i].quality++;
@@ -36,15 +40,15 @@ class GildedRose {
                 }
             }
 
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+            if (!ItemNameIs("Sulfuras, Hand of Ragnaros")) {
                 items[i].sellIn--;
             }
 
             if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (!ItemNameIs("Aged Brie")) {
+                    if (!ItemNameIs("Backstage passes to a TAFKAL80ETC concert")) {
                         if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
+                            if (!ItemNameIs("Sulfuras, Hand of Ragnaros")) {
                                 items[i].quality--;
                             }
                         }
